75b6b4b4c7b114c61efe5b9426f138eb10214e3a
Remya Raj <remya.r@marketsimplified.com>
2019-09-13 09:22:11 +0530

    learning flutter

---
+++ startup_namer/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(StartUpNamer());
+
+class StartUpNamer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: const Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
